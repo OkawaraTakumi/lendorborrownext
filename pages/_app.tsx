ce38be23827495dae4d8e258b5f87bc8acb9a6eb
OkawaraTakumi <takumi.okawara@rakus-partners.co.jp>
2021-09-13 17:09:30 +0900

    'material-UI'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '../redux-app/store'
+import { Head } from 'next/document'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    jssStyles?.parentElement?.removeChild(jssStyles)
+  }, []) 
+
   return (
   <>
+    <Head>
+        <title>貸し借りDB</title>
+    </Head>
     <Provider store={store}>
         <Component {...pageProps} />
     </Provider>
